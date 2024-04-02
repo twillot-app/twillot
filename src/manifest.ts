788836925de12c837efec6358565f4f13df2f7b5
SiZapPaaiGwat <simon@lionjs.com>
2024-04-03 00:28:04 +0800

    Add pages folder

---
+++ src/manifest.ts
@@ -0,0 +1,47 @@
+import { defineManifest } from '@crxjs/vite-plugin'
+import packageData from '../package.json'
+
+//@ts-ignore
+const isDev = process.env.NODE_ENV == 'development'
+
+export default defineManifest({
+  name: `${packageData.displayName || packageData.name}${isDev ? ` ➡️ Dev` : ''}`,
+  description: packageData.description,
+  version: packageData.version,
+  manifest_version: 3,
+  icons: {
+    16: 'img/logo-16.png',
+    32: 'img/logo-34.png',
+    48: 'img/logo-48.png',
+    128: 'img/logo-128.png',
+  },
+  action: {
+    default_popup: 'pages/popup.html',
+    default_icon: 'img/logo-48.png',
+  },
+  options_page: 'pages/options.html',
+  devtools_page: 'pages/devtools.html',
+  background: {
+    service_worker: 'src/background/index.ts',
+    type: 'module',
+  },
+  content_scripts: [
+    {
+      matches: ['http://*/*', 'https://*/*'],
+      js: ['src/contentScript/index.ts'],
+    },
+  ],
+  side_panel: {
+    default_path: 'pages/sidepanel.html',
+  },
+  web_accessible_resources: [
+    {
+      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+      matches: [],
+    },
+  ],
+  permissions: ['sidePanel', 'storage'],
+  chrome_url_overrides: {
+    newtab: 'pages/newtab.html',
+  },
+})

9034762bfc82f5554fa4cd809947154b488ea0bb
SiZapPaaiGwat <xugaofan@gmail.com>
2024-04-03 20:34:38 +0800

    change layout

--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -1,4 +1,4 @@
-import { createEffect, For } from 'solid-js'
+import { createEffect, For, Show } from 'solid-js'
 import dataStore from './store'
 
 import { searchBookmark, syncAllBookmarks } from '../libs/bookmark'
@@ -40,42 +40,60 @@ export const Options = () => {
   })
 
   return (
-    <main>
-      <div class="flex flex-col items-center max-w-2xl mx-auto">
-        <p>
-          {store.isAuthFailed
-            ? 'Authentication failed. Please log in again.'
-            : ''}
-        </p>
-        <div class="flex">
-          <input
-            onChange={(e) => setStore('keyword', e.target.value as string)}
-            placeholder="Search for a tweet now"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
-          <button onClick={query}>Search</button>
+    <main class="flex flex-col items-center max-w-2xl mx-auto h-screen">
+      <p>
+        {store.isAuthFailed
+          ? 'Authentication failed. Please log in again.'
+          : ''}
+      </p>
+      <div class="flex w-full mt-4">
+        <input
+          onChange={(e) => setStore('keyword', e.target.value as string)}
+          placeholder="Search for a tweet now"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white flex-1 outline-none"
+        />
+        <button
+          onClick={query}
+          class="ml-2 w-20 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
+        >
+          Search
+        </button>
+      </div>
+      <Show when={!!store.keyword.trim()}>
+        <div class="mt-4 text-base text-gray-500 text-left w-full">
+          Find {store.tweets.length} records: <strong>{store.keyword}</strong>
         </div>
-
-        <div class="">
-          <For each={store.tweets}>
-            {(tweet) => (
-              <div class="hover:bg-black hover:bg-opacity-5 rounded-lg p-4">
-                <div class="flex flex-shrink-0 pb-0">
-                  <div class="flex items-start cursor-pointer">
-                    <div
-                      class="mr-2"
-                      onClick={() =>
-                        openNewTab(`https://twitter.com/${tweet.screen_name}/`)
-                      }
-                    >
-                      <img
-                        class="inline-block h-10 w-10 rounded-full"
-                        src={tweet.avatar_url}
-                        alt="avatar"
-                      />
-                    </div>
-                    <div class="cursor-pointer">
-                      <p class="text-base leading-6 font-bold text-black dark:text-white">
+      </Show>
+      <div class="my-4 flex-1 overflow-y-auto">
+        <For each={store.tweets}>
+          {(tweet) => (
+            <div class="hover:bg-black hover:bg-opacity-5 p-2">
+              <div class="flex flex-shrink-0 pb-0">
+                <div class="flex items-start cursor-pointer">
+                  <div
+                    class="mr-2"
+                    onClick={() =>
+                      openNewTab(`https://twitter.com/${tweet.screen_name}/`)
+                    }
+                  >
+                    <img
+                      class="inline-block h-10 w-10 rounded-full"
+                      src={tweet.avatar_url}
+                      alt="avatar"
+                    />
+                  </div>
+                  <div class="cursor-pointer">
+                    <p class="text-base leading-6 font-bold text-black dark:text-white">
+                      <span
+                        onClick={() =>
+                          openNewTab(
+                            `https://twitter.com/${tweet.screen_name}/`,
+                          )
+                        }
+                      >
+                        {tweet.username}&nbsp;
+                      </span>
+                      <span class="text-sm leading-5 ml-1 font-normal text-[rgb(83,100,113)] dark:text-white">
                         <span
                           onClick={() =>
                             openNewTab(
@@ -83,46 +101,35 @@ export const Options = () => {
                             )
                           }
                         >
-                          {tweet.username}&nbsp;
+                          @{tweet.screen_name} ·{' '}
                         </span>
-                        <span class="text-sm leading-5 ml-1 font-normal text-[rgb(83,100,113)] dark:text-white">
-                          <span
-                            onClick={() =>
-                              openNewTab(
-                                `https://twitter.com/${tweet.screen_name}/`,
-                              )
-                            }
-                          >
-                            @{tweet.screen_name} ·{' '}
-                          </span>
-                          <span
-                            onClick={() =>
-                              openNewTab(
-                                `https://twitter.com/${tweet.screen_name}/status/${tweet.tweet_id}`,
-                              )
-                            }
-                          >
-                            {new Date(tweet.created_at * 1000).toLocaleString()}
-                          </span>
+                        <span
+                          onClick={() =>
+                            openNewTab(
+                              `https://twitter.com/${tweet.screen_name}/status/${tweet.tweet_id}`,
+                            )
+                          }
+                        >
+                          {new Date(tweet.created_at * 1000).toLocaleString()}
                         </span>
-                      </p>
-                    </div>
+                      </span>
+                    </p>
                   </div>
                 </div>
-                <div class="pl-12 -mt-2">
-                  <p class="text-base width-auto font-medium text-black dark:text-white flex-shrink">
-                    {tweet.full_text.split('\n').map((i) => (
-                      <>
-                        <span>{i}</span>
-                        <br />
-                      </>
-                    ))}
-                  </p>
-                </div>
               </div>
-            )}
-          </For>
-        </div>
+              <div class="pl-12 -mt-2">
+                <p class="text-base width-auto font-medium text-black dark:text-white flex-shrink">
+                  {tweet.full_text.split('\n').map((i) => (
+                    <>
+                      <span>{i}</span>
+                      <br />
+                    </>
+                  ))}
+                </p>
+              </div>
+            </div>
+          )}
+        </For>
       </div>
     </main>
   )

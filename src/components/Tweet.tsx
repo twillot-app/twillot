2e3510385a182061925f5480e18114e28b205f40
SiZapPaaiGwat <simon@lionjs.com>
2024-04-05 01:18:17 +0800

    Enhance Tweet style

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,16 +1,18 @@
 export function Text({ text, keyword }: { text: string, keyword?: string }) {
-  const kRegex = keyword? `|${keyword}` : '';
-  const regex = new RegExp(`(@\\w+|https:\\/\\/[^\s]+|#[^\s#]+${kRegex})`, 'gi');
+  const regex = new RegExp(`(@\\w+|https?:\\/\\/[\\w\\d\\.\\-\\/_?=&#%]+|#[^\s#]+)`, 'gi');
   const parts = text.split(regex);
 
   return (
     <span>
       {parts.map((part) => {
         if (part.startsWith('@') || part.startsWith('https') || part.startsWith('#')) {
-          return <span class="text-blue-400 cursor-pointer" data-text={part}>{part}</span>;
-        } else if (keyword && part.toLowerCase() === keyword.toLowerCase()) {
-          // bg-color 不生效
-          return <span class="cursor-text" style={{ 'background-color': 'yellowgreen' }}>{part}</span>;
+          return <span class="text-blue-400 cursor-pointer" data-text={part} style={{ "background-color": keyword && part.toLowerCase().includes(keyword.toLowerCase())? "yellowgreen" : "inherit" }}>{part}</span>;
+        } else if (keyword && part.toLowerCase().includes(keyword.toLowerCase())) {
+          return part.split(new RegExp(`(${keyword})`, 'gi')).map((subPart) => {
+            return subPart.toLowerCase() === keyword.toLowerCase()
+             ? <span class="cursor-text" style={{ 'background-color': 'yellowgreen' }}>{subPart}</span>
+              : subPart;
+          })
         }
 
         return part;

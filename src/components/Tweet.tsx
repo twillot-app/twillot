f8889d45e4e1503975460fa44862c8cf073ee7d1
SiZapPaaiGwat <simon@lionjs.com>
2024-04-06 02:03:50 +0800

    Fix some regex issue

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,22 +1,55 @@
-export function Text({ text, keyword }: { text: string, keyword?: string }) {
-  const regex = new RegExp(`(@\\w+|https?:\\/\\/[\\w\\d\\.\\-\\/_?=&#%]+|#[^\s#]+)`, 'gi');
-  const parts = text.split(regex);
+export function Text({ text, keyword }: { text: string; keyword?: string }) {
+  const at = '@\\w+'
+  const link = 'https?:\\/\\/[\\w\\d\\.\\-\\/_?=&#%]+'
+  const hashtag = '#(?!\\s|$)[^\\s#]+'
+  const regex = new RegExp(`(${at}}|${link}|${hashtag})`, 'gi')
+  const atRegex = new RegExp(`^${at}$`, 'gi')
+  const linkRegex = new RegExp(`^${link}$`, 'gi')
+  const hashtagRegex = new RegExp(`^${hashtag}$`, 'gi')
+  const parts = text.split(regex)
 
   return (
     <span>
       {parts.map((part) => {
-        if (part.startsWith('@') || part.startsWith('https') || part.startsWith('#')) {
-          return <span class="text-blue-400 cursor-pointer" data-text={part} style={{ "background-color": keyword && part.toLowerCase().includes(keyword.toLowerCase())? "yellowgreen" : "inherit" }}>{part}</span>;
-        } else if (keyword && part.toLowerCase().includes(keyword.toLowerCase())) {
+        if (
+          atRegex.test(part) ||
+          linkRegex.test(part) ||
+          hashtagRegex.test(part)
+        ) {
+          return (
+            <span
+              class="text-blue-400 cursor-pointer"
+              data-text={part}
+              style={{
+                'background-color':
+                  keyword && part.toLowerCase().includes(keyword.toLowerCase())
+                    ? 'yellowgreen'
+                    : 'inherit',
+              }}
+            >
+              {part}
+            </span>
+          )
+        } else if (
+          keyword &&
+          part.toLowerCase().includes(keyword.toLowerCase())
+        ) {
           return part.split(new RegExp(`(${keyword})`, 'gi')).map((subPart) => {
-            return subPart.toLowerCase() === keyword.toLowerCase()
-             ? <span class="cursor-text" style={{ 'background-color': 'yellowgreen' }}>{subPart}</span>
-              : subPart;
+            return subPart.toLowerCase() === keyword.toLowerCase() ? (
+              <span
+                class="cursor-text"
+                style={{ 'background-color': 'yellowgreen' }}
+              >
+                {subPart}
+              </span>
+            ) : (
+              subPart
+            )
           })
         }
 
-        return part;
+        return part
       })}
     </span>
-  );
+  )
 }

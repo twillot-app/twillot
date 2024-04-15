66b5578b093b727b4c1c57ca4072d2202305529e
SiZapPaaiGwat <simon@lionjs.com>
2024-04-15 23:02:55 +0800

    Replace mem card with notion under development card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,12 @@
 export default function Card(props) {
-  const { header, label, footer } = props as any
   return (
-    <div class="flex gap-4 flex-col w-[42rem] mx-auto mt-4 text-sm">
-      <div class="shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring justify-between p-0 rounded-xl border border-input flex flex-col">
+    <div class="flex gap-4 flex-col w-full mx-auto mt-4 text-base text-black dark:text-white">
+      <div class="shadow-sm focus-visible:outline-none justify-between p-0 rounded-xl border flex flex-col border-gray-200 dark:border-gray-500">
         <div class="flex flex-col pb-0 w-full p-3 px-4">
           <h3 class="font-medium relative">{props.header}</h3>
-          <div class="mt-1">{props.label}</div>
+          <div class="mt-1 text-gray-400 text-sm">{props.label}</div>
         </div>
-        <div class="flex w-full justify-end border-t mt-4 border-border p-2 px-3.5 gap-4">
+        <div class="flex w-full justify-end border-t border-gray-200 dark:border-gray-500 mt-4 p-2 px-3.5 gap-4">
           {props.footer}
         </div>
       </div>
--- src/options/Export.tsx
+++ src/options/Export.tsx
@@ -3,20 +3,17 @@ import { openPage } from '../libs/dom'
 import { EXPORT_FORMAT } from '../libs/export'
 import Card from '../components/Card'
 import Button from '../components/Button'
-import { addLocalItem } from '../libs/browser'
-import dataStore from './store'
-import { mem } from '../libs/sync'
+// import dataStore from './store'
 
 export default function Export() {
-  const [store, addStore] = dataStore
+  // const [store, addStore] = dataStore
   return (
     <div class="flex gap-4 flex-col w-[42rem] mx-auto mt-4 text-base">
       <Card
         header="Export / Local file"
         label={
           <>
-            {' '}
-            Note: Media files will be exported as links. <br />
+            Media files will be exported as links. <br />
             If you want to download them, please leave a comment
             <span
               data-text="https://dub.sh/PhqAQ4l"
@@ -40,13 +37,24 @@ export default function Export() {
         }
       />
       <Card
-        header="Sync / Mem"
+        header="Sync / Notion"
         label={
           <>
-            <input
+            This feature is under development. <br />
+            Leave a comment
+            <span
+              data-text="https://dub.sh/PhqAQ4l"
+              class="cursor-pointer text-blue-500"
+              onClick={openPage}
+            >
+              {' '}
+              here
+            </span>{' '}
+            to fasten the development.
+            {/* <input
               class="w-full border rounded-md px-3 py-2 mt-2 focus:outline-none"
               type="password"
-              placeholder="Sync all all tweets to Mem via API"
+              placeholder="Sync all tweets to Notion"
               value={store.memApiKey}
               onBlur={async (e) => {
                 const key = await addLocalItem('MEM_API_KEY', e.target.value)
@@ -61,20 +69,10 @@ export default function Export() {
               onClick={openPage}
             >
               Get my Mem API key
-            </div>
+            </div> */}
           </>
         }
-        footer={
-          <Button
-            onClick={async () => {
-              for await (const tweet of mem()) {
-                console.log(tweet)
-              }
-            }}
-          >
-            Sync Now
-          </Button>
-        }
+        footer={<Button disabled>Sync Now</Button>}
       />
     </div>
   )

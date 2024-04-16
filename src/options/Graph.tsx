74663a653f0e5a738d7bd54709d2ed442e5ad5b9
SiZapPaaiGwat <xugaofan@gmail.com>
2024-04-16 14:20:55 +0800

    Fix render error

--- src/options/Graph.tsx
+++ src/options/Graph.tsx
@@ -1,4 +1,4 @@
-import { onMount, For } from 'solid-js'
+import { onMount, For, Show } from 'solid-js'
 import TopN from '../components/TopN'
 import dataStore from './store'
 
@@ -12,23 +12,28 @@ export default function Graph() {
     <div class="w-[42rem] text-base">
       <div>
         <h2 class="text-lg font-bold text-center my-4">
-          Top 10 Twitter Authors you loved
+          Top 10 Authors you loved
         </h2>
         <div class="flex">
-          <TopN users={store.topUsers} stageSize={480} />
-          <ul class="flex-1 flex flex-col">
+          <Show
+            when={store.topUsers.length > 0}
+            fallback={<div class="w-[480px] h-[480px]"></div>}
+          >
+            <TopN users={store.topUsers} stageSize={480} />
+          </Show>
+          <ul class="flex-1 flex flex-col ml-4">
             <For each={store.topUsers}>
-              {(user) => {
+              {(user, index) => {
                 return (
-                  <li class="flex mt-2 justify-between items-center">
+                  <li
+                    class="flex mt-2 justify-between items-center"
+                    title={user.username}
+                  >
+                    <span class="italic w-8 font-medium">#{index() + 1}</span>
                     <img src={user.avatar_url} class="w-10 h-10 rounded-full" />
-                    <span
-                      class="text-xs mx-2 max-w-28 overflow-hidden text-ellipsis whitespace-nowrap inline-block h-4 leading-4"
-                      title={user.username}
-                    >
-                      {user.username}
-                    </span>
-                    <span class="italic font-medium">{user.count}</span>
+                    <p class="flex-1 flex flex-col text-right">
+                      <span class="italic">{user.count}</span>
+                    </p>
                   </li>
                 )
               }}
--- src/options/Home.tsx
+++ src/options/Home.tsx
@@ -23,12 +23,12 @@ export const Home = () => {
       ref={listRef!}
     >
       <div class="text-gray-900 bg-black bg-opacity-5 dark:bg-gray-800 dark:text-white dark:bg-opacity-100 p-4 rounded-md relative">
-        <h2 class="text-xl font-bold">Top 10 Users from your bookmarks</h2>
+        <h2 class="text-lg font-bold">Top 10 Authors from your bookmarks</h2>
         <A
           href="/graph"
           class="flex absolute top-4 right-4 text-sm text-blue-500 hover:text-blue-700"
         >
-          More
+          Details
         </A>
         <div class="flex mt-4 justify-between">
           <For each={store.topUsers}>

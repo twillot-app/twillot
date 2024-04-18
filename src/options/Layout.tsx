6b1a59cfaaba516922db4646d11c5c47e806a29d
SiZapPaaiGwat <xugaofan@gmail.com>
2024-04-18 11:49:25 +0800

    Move logic to handlers

--- src/options/Layout.tsx
+++ src/options/Layout.tsx
@@ -1,31 +1,16 @@
 import { createEffect, onMount, Show } from 'solid-js'
 
 import dataStore from './store'
-import { searchBookmark, syncAllBookmarks } from '../libs/bookmark'
-import { AuthStatus, Header } from '../types'
-import { getAuthInfo } from '../libs/browser'
-import { countRecords, getTopUsers } from '../libs/db'
 import Indicator from '../components/Indicator'
 import Authenticate from './Authenticate'
 import Search from './Search'
 import Tabs from './Tabs'
 import { useSearchParams } from '@solidjs/router'
-import { parseTwitterQuery } from '../libs/query-parser'
+import { initSync, query } from './handlers'
 
 export const Layout = (props) => {
   const [searchParams] = useSearchParams()
   const [store, setStore] = dataStore
-  const query = async () => {
-    const q = parseTwitterQuery(store.keyword) as any
-    const start = new Date().getTime()
-    const tweets = await searchBookmark(
-      q.fromUser ? q : store.keyword || '',
-      store.page,
-      store.pageSize,
-    )
-    setStore('tweets', () => [...tweets])
-    setStore('searchTime', new Date().getTime() - start)
-  }
 
   createEffect(async () => {
     if (searchParams.q) {
@@ -34,72 +19,7 @@ export const Layout = (props) => {
   })
 
   createEffect(query)
-
-  onMount(async () => {
-    try {
-      /**
-       * 可能之前已经同步过数据，先获取试试
-       */
-      const topUsers = await getTopUsers(10)
-      setStore('topUsers', topUsers)
-      setStore('totalCount', await countRecords())
-
-      const auth = await getAuthInfo()
-      if (!auth || !auth.cookie) {
-        throw new Error(AuthStatus.AUTH_FAILED)
-      }
-
-      if (!auth.lastSynced) {
-        setStore('isForceSyncing', true)
-        /**
-         * 全量同步时展示最新的 100 条数据
-         * 后续更新只更新总数
-         */
-        for await (const docs of syncAllBookmarks(auth as Header, true)) {
-          /**
-           * 已经有最新的数据展示，不对数据进行操作
-           */
-          if (store.tweets.length > 0) {
-            setStore('totalCount', (val) => val + docs.length)
-          } else {
-            setStore('tweets', () => [...docs])
-          }
-        }
-        setStore('isForceSyncing', false)
-        const topUsers = await getTopUsers(10)
-        setStore('topUsers', topUsers)
-      } else {
-        setStore('syncTime', auth.lastSynced)
-        /**
-         * 增量更新时先展示最新的 100 条
-         * 后续更新同时更新总数和展示数据
-         */
-        setStore('isAutoSyncing', true)
-        for await (const docs of syncAllBookmarks(auth as Header)) {
-          setStore('totalCount', await countRecords())
-          /**
-           * 当前正在搜索时不更新数据
-           */
-          if (store.isAutoSyncing && store.keyword.trim()) {
-            continue
-          }
-          await query()
-        }
-        setStore('isAutoSyncing', false)
-      }
-      const syncedTime = Math.floor(Date.now() / 1000)
-      await chrome.storage.local.set({
-        lastSynced: syncedTime,
-      })
-      setStore('syncTime', syncedTime)
-      setStore('topUsers', await getTopUsers(10))
-    } catch (err) {
-      console.error(err)
-      if (err.message == AuthStatus.AUTH_FAILED) {
-        setStore('isAuthFailed', true)
-      }
-    }
-  })
+  onMount(initSync)
 
   return (
     <main class="bg-white dark:bg-black text-black dark:text-white">
--- src/options/handlers.ts
+++ src/options/handlers.ts
@@ -1,5 +1,10 @@
 import { createPopup, getAuthInfo } from '../libs/browser'
 import { ActionPage } from '../types'
+import { parseTwitterQuery } from '../libs/query-parser'
+import dataStore from './store'
+import { searchBookmark, syncAllBookmarks } from '../libs/bookmark'
+import { AuthStatus, Header } from '../types'
+import { countRecords, getTopUsers } from '../libs/db'
 
 export async function removeBookmark(tweet_id: string) {
   try {
@@ -13,5 +18,85 @@ export async function removeBookmark(tweet_id: string) {
     await createPopup(url)
   } catch (err) {
     console.error(err)
+  }
+}
+
+export async function query() {
+  const [store, setStore] = dataStore
+  const q = parseTwitterQuery(store.keyword) as any
+  const start = new Date().getTime()
+  const tweets = await searchBookmark(
+    q.fromUser ? q : store.keyword || '',
+    store.page,
+    store.pageSize,
+  )
+  setStore('tweets', () => [...tweets])
+  setStore('searchTime', new Date().getTime() - start)
+}
+
+export async function initSync() {
+  const [store, setStore] = dataStore
+  try {
+    /**
+     * 可能之前已经同步过数据，先获取试试
+     */
+    const topUsers = await getTopUsers(10)
+    setStore('topUsers', topUsers)
+    setStore('totalCount', await countRecords())
+
+    const auth = await getAuthInfo()
+    if (!auth || !auth.cookie) {
+      throw new Error(AuthStatus.AUTH_FAILED)
+    }
+
+    if (!auth.lastSynced) {
+      setStore('isForceSyncing', true)
+      /**
+       * 全量同步时展示最新的 100 条数据
+       * 后续更新只更新总数
+       */
+      for await (const docs of syncAllBookmarks(auth as Header, true)) {
+        /**
+         * 已经有最新的数据展示，不对数据进行操作
+         */
+        if (store.tweets.length > 0) {
+          setStore('totalCount', (val) => val + docs.length)
+        } else {
+          setStore('tweets', () => [...docs])
+        }
+      }
+      setStore('isForceSyncing', false)
+      const topUsers = await getTopUsers(10)
+      setStore('topUsers', topUsers)
+    } else {
+      setStore('syncTime', auth.lastSynced)
+      /**
+       * 增量更新时先展示最新的 100 条
+       * 后续更新同时更新总数和展示数据
+       */
+      setStore('isAutoSyncing', true)
+      for await (const docs of syncAllBookmarks(auth as Header)) {
+        setStore('totalCount', await countRecords())
+        /**
+         * 当前正在搜索时不更新数据
+         */
+        if (store.isAutoSyncing && store.keyword.trim()) {
+          continue
+        }
+        await query()
+      }
+      setStore('isAutoSyncing', false)
+    }
+    const syncedTime = Math.floor(Date.now() / 1000)
+    await chrome.storage.local.set({
+      lastSynced: syncedTime,
+    })
+    setStore('syncTime', syncedTime)
+    setStore('topUsers', await getTopUsers(10))
+  } catch (err) {
+    console.error(err)
+    if (err.message == AuthStatus.AUTH_FAILED) {
+      setStore('isAuthFailed', true)
+    }
   }
 }

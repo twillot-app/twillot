524c2e89ed4067db92a6dfedf0515374ffe10441
SiZapPaaiGwat <xugaofan@gmail.com>
2024-04-23 20:24:06 +0800

    Using Vertical Tabs

--- src/options/Tabs.tsx
+++ src/options/Tabs.tsx
@@ -3,7 +3,7 @@ import { A } from '@solidjs/router'
 const tabs = [
   {
     url: '/',
-    label: 'Tweets',
+    label: 'Home',
   },
   {
     url: '/graph',
@@ -21,14 +21,14 @@ const tabs = [
 
 export default function Tabs() {
   return (
-    <ul class="w-[42rem] flex flex-wrap text-base font-medium text-center texb-black border-b border-gray-200 dark:border-gray-500 dark:text-white mt-4">
+    <ul class="fixed left-[calc(50vw-36rem)] top-[102px] mt-4 flex w-40 flex-col flex-wrap text-center text-lg font-medium text-black dark:text-white">
       {tabs.map((tab) => (
-        <li class="me-2">
+        <li class="me-2 flex w-full">
           <A
             href={tab.url}
-            class="inline-block px-4 py-2 rounded-t-lg"
+            class="block w-full px-4 py-2"
             inactiveClass="hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-            activeClass="text-blue-500 bg-gray-100 dark:bg-gray-800 dark:text-blue-500 active"
+            activeClass="text-blue-500 active"
             end
           >
             {tab.label}

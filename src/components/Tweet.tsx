6d63f0af7a629cb5425ccdf9ab6c3e8624e836b7
SiZapPaaiGwat <xugaofan@gmail.com>
2024-04-25 15:37:06 +0800

    Display quoted tweet

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,10 +1,12 @@
 import { createMemo, createSignal, Show } from 'solid-js'
+
 import {
   escapeHtml,
   escapeRegExp,
   highlightAndLinkify,
   maxChars,
 } from '../libs/text'
+import { Media, Tweet, TweetQuoted } from '../types'
 
 export const FullText = (props: { text: string; keyword?: string }) => {
   const [showFullText, setShowFullText] = createSignal(
@@ -28,7 +30,7 @@ export const FullText = (props: { text: string; keyword?: string }) => {
         <>
           <span class="whitespace-pre-wrap break-words" innerHTML={html()} />
           <div
-            class="text-blue-500 cursor-pointer mt-2"
+            class="mt-2 cursor-pointer text-blue-500"
             onClick={() => setShowFullText(true)}
           >
             {matches > 1 ? `Show all ${matches} matches` : 'Show full text'}
@@ -36,7 +38,75 @@ export const FullText = (props: { text: string; keyword?: string }) => {
         </>
       }
     >
-      <span class="break-words whitespace-pre-wrap" innerHTML={html()} />
+      <span class="whitespace-pre-wrap break-words" innerHTML={html()} />
     </Show>
+  )
+}
+
+export const MediaItems = (props: { media_items: Media[] }) => {
+  const width = props.media_items.length > 1 ? 'calc(50%-4px)' : '100%'
+  return props.media_items.map((item) => (
+    <div class={`relative flex w-[${width}] items-center`}>
+      <Image
+        src={item.media_url_https}
+        alt={item.ext_alt_text}
+        url={
+          item.video_info
+            ? item.video_info.variants[item.video_info.variants.length - 1].url
+            : ''
+        }
+      />
+      <Show when={item.type === 'video'}>
+        <div class="pointer-events-none absolute left-1/2 top-1/2 -ml-8 -mt-8 flex h-14 w-14 items-center justify-center rounded-full">
+          <svg viewBox="0 0 60 61" aria-hidden="true">
+            <g>
+              <circle
+                cx="30"
+                cy="30.4219"
+                fill="#333333"
+                opacity="0.6"
+                r="30"
+              ></circle>
+              <path
+                d="M22.2275 17.1971V43.6465L43.0304 30.4218L22.2275 17.1971Z"
+                fill="white"
+              ></path>
+            </g>
+          </svg>
+        </div>
+      </Show>
+    </div>
+  ))
+}
+
+export function Image(props: { src: string; alt?: string; url?: string }) {
+  return (
+    <figure
+      class="h-auto w-full cursor-pointer grayscale filter transition-all duration-300 hover:grayscale-0"
+      title={props.alt || ''}
+    >
+      <a href={props.url || props.src} target="_blank">
+        <img class="rounded-lg" src={props.src} />
+      </a>
+    </figure>
+  )
+}
+
+export const Content = (props: {
+  tweet: Tweet | TweetQuoted
+  keyword?: string
+}) => {
+  const tweet = props.tweet
+  return (
+    <>
+      <div class="width-auto text-base font-normal leading-6 ">
+        <FullText text={tweet.full_text} keyword={props.keyword} />
+      </div>
+      <Show when={tweet.media_items}>
+        <div class="my-2 flex flex-wrap space-x-1 space-y-1">
+          <MediaItems media_items={tweet.media_items} />
+        </div>
+      </Show>
+    </>
   )
 }
--- src/options/Home.tsx
+++ src/options/Home.tsx
@@ -1,8 +1,8 @@
-import { createEffect, For } from 'solid-js'
+import { createEffect, For, Show } from 'solid-js'
 import { A } from '@solidjs/router'
 
 import dataStore from './store'
-import { FullText } from '../components/Tweet'
+import { Content } from '../components/Tweet'
 import { openPage } from '../libs/dom'
 import { Host } from '../types'
 import Contribution from '../components/Contribution'
@@ -19,7 +19,7 @@ export const Home = () => {
 
   return (
     <div
-      class="my-4 w-[48rem] flex-1 overflow-y-auto text-base text-black dark:text-white"
+      class="my-4 w-[48rem] flex-1 overflow-y-auto overflow-x-hidden text-base text-black dark:text-white"
       onClick={openPage}
       ref={listRef!}
     >
@@ -83,12 +83,13 @@ export const Home = () => {
               <div class="flex flex-shrink-0 pb-0">
                 <div class="flex cursor-pointer items-start">
                   <div class="mr-2">
-                    <img
-                      class="inline-block h-10 w-10 rounded-full"
-                      src={tweet.avatar_url.replace('_normal', '_x96')}
-                      data-text={`${Host}/${tweet.screen_name}/`}
-                      alt="avatar"
-                    />
+                    <A href={`/?q=from:${tweet.screen_name}`}>
+                      <img
+                        class="inline-block h-10 w-10 rounded-full"
+                        src={tweet.avatar_url.replace('_normal', '_x96')}
+                        alt="avatar"
+                      />
+                    </A>
                   </div>
                   <div class="cursor-pointer">
                     <p class="text-base font-bold leading-6 text-black dark:text-white">
@@ -110,10 +111,25 @@ export const Home = () => {
                   </div>
                 </div>
               </div>
-              <div class="-mt-2 pl-12">
-                <div class="width-auto text-base font-normal leading-6 text-[rgb(15,20,25)] dark:text-white">
-                  <FullText text={tweet.full_text} keyword={store.keyword} />
-                </div>
+              <div class="-mt-2 pl-12 text-[rgb(15,20,25)] dark:text-white">
+                <Content tweet={tweet} keyword={store.keyword} />
+                <Show when={tweet.quoted_tweet}>
+                  <div class="relative inline-flex w-full items-center justify-center">
+                    <hr class="my-8 h-1 w-64 rounded border-0 bg-gray-200 dark:bg-gray-700" />
+                    <div class="absolute left-1/2 -translate-x-1/2 px-4">
+                      <svg
+                        class="h-4 w-4 text-gray-500 dark:text-gray-300"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 18 14"
+                      >
+                        <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <Content tweet={tweet.quoted_tweet} />
+                </Show>
               </div>
             </div>
           )}

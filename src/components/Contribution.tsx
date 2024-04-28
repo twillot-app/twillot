0f35399abc2f29466f4901aaeef4488c28f935ef
SiZapPaaiGwat <simon@lionjs.com>
2024-04-28 23:45:11 +0800

    Fix contribution

--- src/components/Contribution.tsx
+++ src/components/Contribution.tsx
@@ -2,41 +2,40 @@ import { For } from 'solid-js'
 import { useNavigate } from '@solidjs/router'
 
 import dataStore from '../options/store'
-import { formatDate } from '../libs/date'
+import {
+  WEEK_NAMES,
+  formatDate,
+  getGridDates,
+  getMonthNames,
+} from '../libs/date'
 import { disabledColor, getColorForFavorites } from '../libs/color'
 
 export default function Contribution() {
   const [store] = dataStore
   const navigate = useNavigate()
-
+  const { dates, weekWidth } = getGridDates()
+  const monthNames = getMonthNames(new Date(dates[0]))
+  const gridTemplateColumns = weekWidth
+    .map((w) => `calc(var(--week-width) * ${w})`)
+    .join(' ')
   return (
     <>
       <h3 class="mb-4 text-lg font-medium">
         {store.historySize} bookmarks in the last year
       </h3>
       <div class="graph mx-auto w-full text-xs">
-        <ul class="months leading-3">
-          <li>Jan</li>
-          <li>Feb</li>
-          <li>Mar</li>
-          <li>Apr</li>
-          <li>May</li>
-          <li>Jun</li>
-          <li>Jul</li>
-          <li>Aug</li>
-          <li>Sep</li>
-          <li>Oct</li>
-          <li>Nov</li>
-          <li>Dec</li>
+        <ul
+          class="months leading-3"
+          style={{ 'grid-template-columns': gridTemplateColumns }}
+        >
+          {monthNames.map((monthName) => (
+            <li>{monthName}</li>
+          ))}
         </ul>
         <ul class="days leading-3">
-          <li>Sun</li>
-          <li>Mon</li>
-          <li>Tue</li>
-          <li>Wed</li>
-          <li>Thu</li>
-          <li>Fri</li>
-          <li>Sat</li>
+          {WEEK_NAMES.map((dayName) => (
+            <li>{dayName}</li>
+          ))}
         </ul>
         <ul class="squares">
           <For each={store.history}>
--- src/libs/date.ts
+++ src/libs/date.ts
@@ -1,5 +1,10 @@
 export const DAYS = 52 * 7
 
+export const MONTH_NAMES =
+  'Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec'.split(',')
+
+export const WEEK_NAMES = 'Sun,Mon,Tue,Wed,Thu,Fri,Sat'.split(',')
+
 export function getLastNDaysDates(days: number) {
   const today = new Date()
   const dates = []
@@ -24,4 +29,41 @@ export function formatDate(date: string) {
   const year = d.getFullYear()
 
   return [year, month.padStart(2, '0'), day.padStart(2, '0')].join('-')
+}
+
+export function getGridDates(): {
+  dates: string[]
+  colIndexes: number[]
+  weekWidth: number[]
+} {
+  const dates: string[] = []
+  const colIndexes: number[] = []
+  let currentDate = new Date()
+
+  // 获取当前日期所在周的最后一天（星期六）
+  currentDate = new Date(
+    currentDate.setDate(currentDate.getDate() - currentDate.getDay() + 7),
+  )
+
+  for (let i = 0; i < DAYS; i++) {
+    const date = new Date(currentDate.setDate(currentDate.getDate() - 1))
+    dates.unshift(new Date(date).toLocaleDateString())
+
+    if (date.getDate() === 1) {
+      const monthIndex = DAYS - 1 - i
+      colIndexes.unshift(Math.floor(monthIndex / 7))
+    }
+  }
+
+  colIndexes.unshift(0)
+  const weekWidth = colIndexes.map((col, index) => {
+    return colIndexes[index + 1] - col
+  })
+  weekWidth.pop()
+  return { dates, colIndexes, weekWidth }
+}
+
+export function getMonthNames(date: Date) {
+  const month = date.getMonth()
+  return [...MONTH_NAMES.slice(month), ...MONTH_NAMES.slice(0, month)]
 }

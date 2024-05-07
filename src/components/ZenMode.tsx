903de3b2ae6028b292f35b5f8ae6290fc1162be6
SiZapPaaiGwat <simon@lionjs.com>
2024-05-07 23:24:32 +0800

    Add demo data

--- src/components/ZenMode.tsx
+++ src/components/ZenMode.tsx
@@ -1,19 +1,25 @@
+import { Content } from './Tweet'
+import dataStore from '../options/store'
+import { Show, createMemo } from 'solid-js'
+import { Host } from '../types'
+import FolderSelect from './FolderSelect'
+import { A } from '@solidjs/router'
+
 export default function ZenMode() {
+  const [store] = dataStore
+  const tweet = createMemo(() => store.tweets[0])
   return (
     <div class="fixed inset-0 z-40 flex flex-col bg-white dark:bg-gray-800">
       <header class="mx-auto h-14 w-[60rem] pb-4">
-        <h1>Header</h1>
+        <Show when={tweet()} fallback={null}>
+          Avatar
+        </Show>
       </header>
       <div class="-my-14 flex-1 overflow-auto py-14">
-        <article class="mx-auto w-[60rem] px-2">
-          <p>这里是文章内容，内容可能非常长，需要滚动条来查看全部内容。</p>
-          <p>
-            {new Array(100)
-              .fill(
-                '这是重复的文本，用于创建一个非常长的段落，以演示滚动效果。',
-              )
-              .join(' ')}
-          </p>
+        <article class="mx-auto w-[40rem] px-2">
+          <Show when={tweet()}>
+            <Content tweet={tweet()}></Content>
+          </Show>
         </article>
       </div>
       <footer class="mx-auto h-14 w-[60rem] pt-4">

3cb366c20130e1c00183ec49089e57c4bfdbf415
SiZapPaaiGwat <simon@lionjs.com>
2024-05-08 08:47:58 +0800

    demo

--- src/options/Layout.tsx
+++ src/options/Layout.tsx
@@ -16,6 +16,7 @@ import { Alert } from '../components/Alert'
 import Notification from '../components/Notification'
 import { IconUp } from '../components/Icons'
 import ZenMode from '../components/ZenMode'
+import { Portal } from 'solid-js/web'
 
 export const Layout = (props) => {
   const [searchParams] = useSearchParams()
@@ -40,7 +41,7 @@ export const Layout = (props) => {
   return (
     <main class="bg-white text-black dark:bg-black dark:text-white">
       <Tabs />
-      <div class="fixed left-1/2 top-0 z-30 -ml-[24rem] w-[48rem] bg-white dark:bg-black">
+      <div class="fixed left-1/2 top-0 z-30 -ml-[24rem] hidden w-[48rem] bg-white dark:bg-black">
         <h1 class="font-large my-4 bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-center text-xl font-semibold text-transparent">
           Twillot - your social media copilot
         </h1>
@@ -95,14 +96,16 @@ export const Layout = (props) => {
       <div class="hidden flex-col items-center pt-[102px]">
         {props.children}
       </div>
-      <ZenMode />
-      <Notification />
-      <button
-        class="fixed bottom-10 right-10 z-50  h-14 w-14 rounded-full border-0 bg-purple-500 p-4 text-lg font-semibold text-white opacity-40 shadow-md transition-colors duration-300 hover:bg-purple-700 hover:opacity-100"
-        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-      >
-        <IconUp />
-      </button>
+      <Portal>
+        <ZenMode />
+        <Notification />
+        <button
+          class="fixed bottom-10 right-10 z-50  h-14 w-14 rounded-full border-0 bg-purple-500 p-4 text-lg font-semibold text-white opacity-40 shadow-md transition-colors duration-300 hover:bg-purple-700 hover:opacity-100"
+          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+        >
+          <IconUp />
+        </button>
+      </Portal>
     </main>
   )
 }

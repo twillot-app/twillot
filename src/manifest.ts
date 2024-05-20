a02d2e7ce7536f924ef31bc64bd243ccdc6fcd75
SiZapPaaiGwat <hello@twillot.com>
2024-05-20 14:24:13 +0800

    Add x and twitter both

--- src/manifest.ts
+++ src/manifest.ts
@@ -50,7 +50,7 @@ export default defineManifest({
       matches: [],
     },
   ],
-  host_permissions: ['https://x.com/*'],
+  host_permissions: ['https://x.com/*', 'https://twitter.com/*'],
   permissions: ['storage', 'webRequest', 'cookies', 'tabs'],
   // chrome_url_overrides: {
   //   newtab: 'pages/newtab.html',

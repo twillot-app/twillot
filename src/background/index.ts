fe1ce2d1d7530984573517b5a52d97ceb3ead44d
SiZapPaaiGwat <hello@twillot.com>
2024-05-23 11:23:55 +0800

    Remove tested auto reply

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,15 +1,4 @@
-import { createTweet } from '../libs/api-twitter'
-import {
-  authTwitter,
-  openOptionsPageWhenIconClicked,
-  onBookmarkCreated,
-} from '../libs/browser'
+import { authTwitter, openOptionsPageWhenIconClicked } from '../libs/browser'
 
 openOptionsPageWhenIconClicked()
 authTwitter()
-
-onBookmarkCreated(
-  // TODO 抓取推文内容以及主题，使用 AI 对其进行翻译和总结
-  async (tweet_id: string) =>
-    await createTweet(tweet_id, new Date().toLocaleString()),
-)

a8644dab24b143f2f38d7cf6ac8042be51da913c
SiZapPaaiGwat <simon@lionjs.com>
2024-06-16 00:33:43 +0800

    Remove useless code

--- src/options/Settings.tsx
+++ src/options/Settings.tsx
@@ -1,61 +1,12 @@
-import { createEffect, createSignal } from 'solid-js'
-import { For, onMount } from 'solid-js'
-import dataStore, { mutateStore } from './store'
-import { upsertConfig } from '../libs/db/configs'
-import { OptionName } from '../types'
+import { onMount } from 'solid-js'
 import { getTemplates } from '../libs/workflow/store'
 import SettingsPanel from '../components/SettingsPanel'
 
-const [store, setStore] = dataStore
-
 const Settings = () => {
-  const [newContent, setNewContent] = createSignal('')
-  const [editingIndex, setEditingIndex] = createSignal(-1)
-  const [editingName, setEditingName] = createSignal('')
-  const [editingContent, setEditingContent] = createSignal('')
-
   onMount(async () => {
     getTemplates('COMMENT_TEMPLATE')
     getTemplates('SIGNATURE_TEMPLATE')
   })
-
-  createEffect(async () => {
-    const templates = store.templates.map((t) => ({ ...t }))
-    await upsertConfig({
-      option_name: OptionName.COMMENT_TEMPLATE,
-      option_value: templates,
-    })
-  })
-
-  const addTemplate = (e) => {
-    e.preventDefault()
-    const content = newContent().trim()
-    if (!content || store.templates.some((t) => t.content === content)) {
-      return
-    }
-    const newTemplate = {
-      id: new Date().getTime().toString(16),
-      name: `Untitled ${store.templates.length + 1}`,
-      content,
-      createdAt: Math.floor(new Date().getTime() / 1000),
-    }
-    setStore('templates', [newTemplate, ...store.templates])
-    setNewContent('')
-    setEditingIndex(-1)
-  }
-
-  const saveEdit = (index: number) => {
-    const content = editingContent().trim()
-    if (
-      !content ||
-      store.templates.some((t, i) => t.content === content && i !== index)
-    ) {
-      return
-    }
-    setStore('templates', index, 'name', editingName())
-    setStore('templates', index, 'content', content)
-    setEditingIndex(-1)
-  }
 
   return (
     <div class="container mx-auto p-4 text-base">

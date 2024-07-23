8bd6b2a1f139173df3d8acc68a360d6dcae94dc7
SiZapPaaiGwat <simon@lionjs.com>
2024-07-23 21:47:01 +0800

    Add form

--- x-bookmarks-automation/entrypoints/popup/App.tsx
+++ x-bookmarks-automation/entrypoints/popup/App.tsx
@@ -13,59 +13,64 @@ import { Switch, SwitchControl, SwitchThumb } from '~/components/ui/switch'
 
 export default function App() {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Cookie Settings</CardTitle>
-        <CardDescription>Manage your cookie settings here.</CardDescription>
-      </CardHeader>
-      <CardContent class="grid gap-6">
-        <div class="flex items-center justify-between space-x-2">
-          <Label for="necessary" class="flex flex-col space-y-1">
-            <span>Strictly Necessary</span>
-            <span class="text-muted-foreground font-normal leading-snug">
-              These cookies are essential in order to use the website and use
-              its features.
-            </span>
-          </Label>
-          <Switch id="necessary" defaultChecked>
-            <SwitchControl>
-              <SwitchThumb />
-            </SwitchControl>
-          </Switch>
-        </div>
-        <div class="flex items-center justify-between space-x-2">
-          <Label for="functional" class="flex flex-col space-y-1">
-            <span>Functional Cookies</span>
-            <span class="text-muted-foreground font-normal leading-snug">
-              These cookies allow the website to provide personalized
-              functionality.
-            </span>
-          </Label>
-          <Switch id="functional">
-            <SwitchControl>
-              <SwitchThumb />
-            </SwitchControl>
-          </Switch>
-        </div>
-        <div class="flex items-center justify-between space-x-2">
-          <Label for="performance" class="flex flex-col space-y-1">
-            <span>Performance Cookies</span>
-            <span class="text-muted-foreground font-normal leading-snug">
-              These cookies help to improve the performance of the website.
-            </span>
-          </Label>
-          <Switch id="performance">
-            <SwitchControl>
-              <SwitchThumb />
-            </SwitchControl>
-          </Switch>
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button variant="outline" class="w-full">
-          Save preferences
-        </Button>
-      </CardFooter>
+    <Card class="m-4 w-80">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          const { like, repost, reply } = e.target as HTMLFormElement
+        }}
+      >
+        <CardHeader>
+          <CardTitle>Twillot Bookmark Automation</CardTitle>
+          <CardDescription>管理你的书签收藏自动化设置</CardDescription>
+        </CardHeader>
+        <CardContent class="grid gap-6">
+          <div class="flex items-center justify-between space-x-2">
+            <Label for="necessary" class="flex flex-col space-y-1">
+              <span>Like</span>
+              <span class="text-muted-foreground font-normal leading-snug">
+                当我收藏推文时点赞该推文
+              </span>
+            </Label>
+            <Switch defaultChecked name="like">
+              <SwitchControl>
+                <SwitchThumb />
+              </SwitchControl>
+            </Switch>
+          </div>
+          <div class="flex items-center justify-between space-x-2">
+            <Label class="flex flex-col space-y-1">
+              <span>Repost</span>
+              <span class="text-muted-foreground font-normal leading-snug">
+                当我收藏推文时转发该推文
+              </span>
+            </Label>
+            <Switch defaultChecked name="repost">
+              <SwitchControl>
+                <SwitchThumb />
+              </SwitchControl>
+            </Switch>
+          </div>
+          <div class="flex items-center justify-between space-x-2">
+            <Label class="flex flex-col space-y-1">
+              <span>Reply</span>
+              <span class="text-muted-foreground font-normal leading-snug">
+                当我收藏推文时评论该推文
+              </span>
+            </Label>
+            <Switch name="reply">
+              <SwitchControl>
+                <SwitchThumb />
+              </SwitchControl>
+            </Switch>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button variant="outline" class="w-full" type="submit">
+            Save preferences
+          </Button>
+        </CardFooter>
+      </form>
     </Card>
   )
 }

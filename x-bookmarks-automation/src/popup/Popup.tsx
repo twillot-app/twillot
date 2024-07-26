77a021e402ce24ffba2075527776901b447cc5c6
SiZapPaaiGwat <simon@lionjs.com>
2024-07-26 13:52:46 +0800

    Icon size

--- x-bookmarks-automation/src/popup/Popup.tsx
+++ x-bookmarks-automation/src/popup/Popup.tsx
@@ -59,7 +59,7 @@ const UpgradeLink = (props) => {
       target="_blank"
     >
       <span class="mr-1 text-yellow-400">
-        <svg class="h-5 w-5" viewBox="0 0 24 24">
+        <svg class="h-4 w-4" viewBox="0 0 24 24">
           <path
             fill="currentColor"
             d="M5 16L3 5l5.5 5L12 4l3.5 6L21 5l-2 11zm14 3c0 .6-.4 1-1 1H6c-.6 0-1-.4-1-1v-1h14z"

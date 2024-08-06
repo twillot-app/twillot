27bc37c58ad42a0793c83f985342ff63a757fbba
SiZapPaaiGwat <simon@lionjs.com>
2024-08-06 19:09:29 +0800

    Add api

--- packages/utils/api/twitter-user.ts
+++ packages/utils/api/twitter-user.ts
@@ -3,13 +3,143 @@ import { flatten, request } from './twitter-base'
 import { COMMON_FEATURES } from './twitter-features'
 import { FetchError } from '../xfetch'
 
-export async function getPosts() {}
+export async function getPosts(userId: string, cursor?: string) {
+  try {
+    const variables = {
+      userId,
+      count: 100,
+      cursor: '',
+      includePromotedContent: true,
+      withQuickPromoteEligibilityTweetFields: true,
+      withVoice: true,
+      withV2Timeline: true,
+    }
+    if (cursor) {
+      variables.cursor = cursor
+    }
+    const query = flatten({
+      variables,
+      features: COMMON_FEATURES,
+      fieldToggles: { withArticlePlainText: false },
+    })
+    const json = await request(`${Endpoint.USER_TWEETS}?${query}`, {
+      body: null,
+      method: 'GET',
+    })
 
-export async function getReplies(postId: string) {}
+    return json
+  } catch (e) {
+    if (e.name !== FetchError.TimeoutError && e.name !== FetchError.DataError) {
+      e.name = FetchError.IdentityError
+    }
 
-export async function getMedia(postId: string) {}
+    throw e
+  }
+}
 
-export async function getLikes(postId: string) {}
+export async function getReplies(userId: string, cursor?: string) {
+  try {
+    const variables = {
+      userId,
+      count: 100,
+      cursor: '',
+      includePromotedContent: true,
+      withCommunity: true,
+      withVoice: true,
+      withV2Timeline: true,
+    }
+    if (cursor) {
+      variables.cursor = cursor
+    }
+    const query = flatten({
+      variables,
+      features: COMMON_FEATURES,
+      fieldToggles: { withArticlePlainText: false },
+    })
+    const json = await request(`${Endpoint.USER_TWEETS_AND_REPLIES}?${query}`, {
+      body: null,
+      method: 'GET',
+    })
+
+    return json
+  } catch (e) {
+    if (e.name !== FetchError.TimeoutError && e.name !== FetchError.DataError) {
+      e.name = FetchError.IdentityError
+    }
+
+    throw e
+  }
+}
+
+export async function getMedia(userId: string, cursor?: string) {
+  try {
+    const variables = {
+      userId,
+      count: 100,
+      cursor: '',
+      includePromotedContent: false,
+      withClientEventToken: false,
+      withBirdwatchNotes: false,
+      withVoice: true,
+      withV2Timeline: true,
+    }
+    if (cursor) {
+      variables.cursor = cursor
+    }
+    const query = flatten({
+      variables,
+      features: COMMON_FEATURES,
+      fieldToggles: { withArticlePlainText: false },
+    })
+    const json = await request(`${Endpoint.USER_MEDIA}?${query}`, {
+      body: null,
+      method: 'GET',
+    })
+
+    return json
+  } catch (e) {
+    if (e.name !== FetchError.TimeoutError && e.name !== FetchError.DataError) {
+      e.name = FetchError.IdentityError
+    }
+
+    throw e
+  }
+}
+
+export async function getLikes(userId: string, cursor?: string) {
+  try {
+    const variables = {
+      userId,
+      count: 100,
+      cursor: '',
+      includePromotedContent: false,
+      withClientEventToken: false,
+      withBirdwatchNotes: false,
+      withVoice: true,
+      withV2Timeline: true,
+    }
+    if (cursor) {
+      variables.cursor = cursor
+    }
+    const query = flatten({
+      variables,
+      features: COMMON_FEATURES,
+      fieldToggles: { withArticlePlainText: false },
+    })
+    const json = await request(`${Endpoint.USER_MEDIA}?${query}`, {
+      body: null,
+      method: 'GET',
+    })
+
+    return json
+  } catch (e) {
+    if (e.name !== FetchError.TimeoutError && e.name !== FetchError.DataError) {
+      e.name = FetchError.IdentityError
+    }
+
+    throw e
+  }
+}
 
 export async function getFollowers(userId: string, cursor?: string) {
   try {

4bd06d8083aef5f112073425b252704a10f45666
SiZapPaaiGwat <hello@twillot.com>
2024-09-09 17:32:54 +0800

    Remove files

--- multi-publish/src/options/index.tsx
+++
@@ -1,7 +0,0 @@
-/* @refresh reload */
-import { render } from 'solid-js/web'
-
-import '../assets/main.css'
-import Popup from './Options'
-
-render(() => <Popup />, document.getElementById('app') ?? document.body)
